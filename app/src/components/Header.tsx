480589ec14aa153ff4de26bd7f50675ae16cb47d
thiagofalima <thiagofalima@hotmail.com>
2026-03-02 09:55:52 -0300

    style(header): add style to Header component

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -5,13 +5,20 @@ const HeaderWrapper = styled.div`
     width: 100vw;
     color: white;
     padding: 16px;
-    background-color: blue;
+    background-color: #09456c;
+
+    h1 {
+        font-size: 1.25em;
+        text-align: center;
+        font-weight: 500;
+        letter-spacing: 2px;
+    }
 `
 
 export const Header = () => {
     return (
         <HeaderWrapper>
-            Hello
+            <h1>RTA Soluções</h1>
         </HeaderWrapper>
     )
 }
