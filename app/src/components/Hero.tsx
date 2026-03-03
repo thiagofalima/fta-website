94e78cbe65b3227a35668987d6872289f0a1d7bc
thiagofalima <thiagofalima@hotmail.com>
2026-03-02 22:03:19 -0300

    feat(hero): Add Hero component

---
+++ app/src/components/Hero.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import MyLogo from "../assets/rta_logo.jpeg";
+import { Center } from "@chakra-ui/react";
+
+const HeroWrapper = styled.div`
+  img {
+    width: 250px;
+  }
+`;
+
+export const Hero = () => {
+  return (
+    <HeroWrapper>
+      <Center>
+        <img src={MyLogo} />
+      </Center>
+    </HeroWrapper>
+  );
+};
