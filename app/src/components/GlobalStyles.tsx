a5117c135b3dcbc9a48777f4f0d51af7e7e60ab6
thiagofalima <thiagofalima@hotmail.com>
2026-03-04 11:18:12 -0300

    refactor(hero): migrate to container div - Also import fonts on index.html for previous font loading

--- app/src/components/GlobalStyles.tsx
+++ app/src/components/GlobalStyles.tsx
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Michroma&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+    body {
+        font-family: "Roboto", sans-serif;
+    }
 `;
--- app/src/components/Hero.tsx
+++ app/src/components/Hero.tsx
@@ -1,19 +1,35 @@
 import styled from "styled-components";
 import MyLogo from "../assets/rta_logo.jpeg";
-import { Center } from "@chakra-ui/react";
 
 const HeroWrapper = styled.div`
+  .container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
   img {
     width: 200px;
+  }
+
+  .heading {
+    margin: 1em 0 auto;
+    letter-spacing: 2px;
+    font-size: 1.5em;
+    font-family: "Michroma", sans-serif;
+    font-weight: bold;
+    font-style: normal;
   }
 `;
 
 export const Hero = () => {
   return (
     <HeroWrapper>
-      <Center flexDirection="column">
+      <div className="container">
+        <h1 className="heading">Bem-vindo(a)</h1>
         <img src={MyLogo} />
-      </Center>
+      </div>
     </HeroWrapper>
   );
 };
