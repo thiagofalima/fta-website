f863c92530426fabe973c85c23733825c44ba3c3
thiagofalima <thiagofalima@hotmail.com>
2026-03-08 19:33:00 -0300

    feat: add contact email and whatsapp links

--- app/src/components/Contact.tsx
+++ app/src/components/Contact.tsx
@@ -1,36 +1,63 @@
 import styled from "styled-components";
 import Wrapper from "./Wrapper";
+import emailIcon from "../assets/icons/email.svg";
+import wppIcon from "../assets/icons/wpp.svg";
 
 const Contacts = styled.div`
-    p.contact::before {
-    content: '';
-    display: inline-block;
-    width: 1.125rem;
-    height: 1.125rem;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
+  div.contact {
+    width: 500px;
+    margin: 1em;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 8px;
+  }
 
-    p.email {
-        background-image: url('../assets/icons/email.svg');
-    }
+  img.wpp {
+    width: 1em;
+  }
+  img.email {
+    width: 1.15em;
+  }
 
-    p.wpp {
-        background-image: url('../assets/icons/wpp.svg');
-    }
-`
+  a {
+  font-weight: 500;
+    letter-spacing: 1.5px;
+    color: #344969;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+`;
 
 export const Contact = () => {
   return (
     <Wrapper>
       <h2>Contato</h2>
       <Contacts>
-        <p className="contact email">
-          E-mail: <a href="#" className="link">contato@rtasolutions.com.br</a>
-        </p>
-        <p className="contact wpp">
-          WhatsApp: <a href="#" className="link">(11)996916400</a>
-        </p>
+        <div className="contact">
+          <img className="email" src={emailIcon} />
+          E-mail:{" "}
+          <p className="contact email">
+            <a href="mailto:contato@rtasolutions.com.br" className="link">
+              contato@rtasolutions.com.br
+            </a>
+          </p>
+        </div>
+        <div className="contact">
+          <img className="wpp" src={wppIcon} />
+          <p className="contact wpp">
+            WhatsApp:{" "}
+            <a
+              href="https://api.whatsapp.com/send?phone=1199696400/message=teste"
+              className="link"
+            >
+              (11)996916400
+            </a>
+          </p>
+        </div>
       </Contacts>
     </Wrapper>
   );
