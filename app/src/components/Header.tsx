fc3f209bc24782ea37c15df677721edb7176b4d2
thiagofalima <thiagofalima@hotmail.com>
2026-03-11 10:14:55 -0300

    feat(header): add Link to different setions - also add on hover behaviour on the Links

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
+import { HashLink as Link } from "react-router-hash-link";
 
 const HeaderWrapper = styled.header`
   width: 100vw;
   color: white;
   padding: 16px;
   background-color: #344969;
+  border-bottom: 2px solid #ffffff;
+  position: sticky;
+  top: 0;
 
   h1 {
     font-family: Michroma, sans-serif;
@@ -22,6 +26,12 @@ const HeaderWrapper = styled.header`
 
   li.link {
     letter-spacing: 2px;
+    transition: opacity 0.5s, text-decoration 1s
+  }
+
+  li.link:hover {
+    opacity: 0.6;
+    text-decoration: underline;
   }
 `;
 
@@ -30,9 +40,21 @@ export const Header = () => {
     <HeaderWrapper>
       <nav className="nav">
         <ul className="nav__links">
-          <li className="link">Sobre</li>
-          <li className="link">Soluções</li>
-          <li className="link">Contato</li>
+          <li className="link">
+            <Link smooth to="#About">
+              Sobre
+            </Link>
+          </li>
+          <li className="link">
+            <Link smooth to="#WhatWeDo">
+              Soluções
+            </Link>
+          </li>
+          <li className="link">
+            <Link smooth to="#Contact">
+              Contato
+            </Link>
+          </li>
         </ul>
       </nav>
     </HeaderWrapper>
